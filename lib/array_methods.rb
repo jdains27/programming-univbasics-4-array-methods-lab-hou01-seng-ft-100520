48647d1e95e48b48870c67196f402fbfe6ffa458
Jordan Dains <dains.jordan2@gmail.com>
2020-09-10 13:35:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,11 +3,11 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-  arrat.sort(array)
+  arrat.sort
 end
 
 def using_reverse(array)
-  array.reverse(array)
+  array.reverse
 end
 
 def using_first(array)
